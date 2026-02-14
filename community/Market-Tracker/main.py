b815b18c7f16f0885d77d94be253efbf40cec184
Akio9090-dev <kana72867@gmail.com>
2026-02-14 23:19:39 +0500

    main.py
    Signed-off-by: Akio9090-dev <kana72867@gmail.com>

--- community/Market-Tracker/main.py
+++ community/Market-Tracker/main.py
@@ -1,11 +1,12 @@
 import json
 import os
 import re
-import requests
 from typing import ClassVar, List
+
+import requests
 from src.agent.capability import MatchingCapability
-from src.main import AgentWorker
 from src.agent.capability_worker import CapabilityWorker
+from src.main import AgentWorker
 
 # Voice ID for "American, Mid-aged, Male, News" (used for strict decimal pronunciation)
 VOICE_ID = "29vD33N1CtxCmqQRPOHJ"
