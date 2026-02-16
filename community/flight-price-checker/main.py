ef5b4f59dba7a5cb9aad6a9e7f61b195ad25e3e1
Md. Reyad Hossain <fiction@Mds-MacBook-Air.local>
2026-02-16 18:26:10 +0600

    Extra new lines removed

--- community/flight-price-checker/main.py
+++ community/flight-price-checker/main.py
@@ -1,13 +1,11 @@
 import json
 import os
 from typing import ClassVar, Set
-
 import requests
 
 from src.agent.capability import MatchingCapability
 from src.agent.capability_worker import CapabilityWorker
 from src.main import AgentWorker
-
 
 class FlightFinderCapability(MatchingCapability):
     @classmethod
