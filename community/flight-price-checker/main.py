678d53ddf5db9539a05e988b0f3e36b358cc7da7
Md. Reyad Hossain <fiction@Mds-MacBook-Air.local>
2026-02-16 18:53:05 +0600

    extra space removed

--- community/flight-price-checker/main.py
+++ community/flight-price-checker/main.py
@@ -20,7 +20,7 @@ class FlightFinderCapability(MatchingCapability):
     worker: AgentWorker = None
     capability_worker: CapabilityWorker = None
 
-    # {{register capability}}
+    #{{register capability}}
 
     API_URL_BASE: ClassVar[str] = "https://kiwi-com-cheap-flights.p.rapidapi.com"
     API_KEY: ClassVar[str] = "API_KEY"
