e5f572c8ea302b592514a609940ae1fd3632cca8
Cedar Scarlett <cedarscarlettsoftware@gmail.com>
2026-02-21 17:10:28 -0800

    fixed register capability tag

--- community/gdrive-voice-manager/main.py
+++ community/gdrive-voice-manager/main.py
@@ -86,17 +86,7 @@ class GDriveVoiceManager(MatchingCapability):
     # Registration
     # =========================================================================
 
-    @classmethod
-    def register_capability(cls) -> "MatchingCapability":
-        with open(
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
-        ) as file:
-            data = json.load(file)
-
-        return cls(
-            unique_name=data["unique_name"],
-            matching_hotwords=data["matching_hotwords"],
-        )
+    #{{register capability}}
 
     # =========================================================================
     # Entry Point
