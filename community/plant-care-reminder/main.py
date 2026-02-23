3b062feab9a0e494dd35ac868a17385807be834c
Kaushal-205 <kaushaldesai205@gmail.com>
2026-02-24 02:16:32 +0530

    fix: restore exact register capability tag

--- community/plant-care-reminder/main.py
+++ community/plant-care-reminder/main.py
@@ -79,7 +79,7 @@ class PlantCareReminderCapability(MatchingCapability):
     capability_worker: CapabilityWorker = None
 
     # Do not change following tag of register capability
-    # {{register capability}}
+    #{{register capability}}
 
     def call(self, worker: AgentWorker):
         self.worker = worker
